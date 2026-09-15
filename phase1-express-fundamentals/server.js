d7c584385953ec3f8d5c65bfd85f4c2b80ed80f1
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-15 11:35:07 +0545

    feat: add POST, PUT, PATCH, DELETE methods to users routing

--- phase1-express-fundamentals/server.js
+++ phase1-express-fundamentals/server.js
@@ -14,6 +14,22 @@ app.get('/users', (req, res)=>{
 app.get('/products', (req,res)=>{
     res.send('Hello from products page')
 })
+//more of http methods like post ,put, delete, patch 
+app.post("/users", (req, res) => {
+  res.send("Create user");
+});
+
+app.put("/users/123", (req, res) => {
+  res.send("Replace user 123");
+});
+
+app.patch("/users/123", (req, res) => {
+  res.send("Update part of user 123");
+});
+
+app.delete("/users/123", (req, res) => {
+  res.send("Delete user 123");
+});
 app.listen(3000, () => {
     console.log('Server running at PORT: 3000')
 })
