3a37a2e7b360b0303a64465c26df6f9fc678abfa
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-15 11:21:10 +0545

    feat: add basic GET route for root endpoint

--- phase1-express-fundamentals/server.js
+++ phase1-express-fundamentals/server.js
@@ -1,5 +1,10 @@
 import express from 'express'
 const app = express()
+//define a get route for home page
+app.get('/', (req,res)=> {
+    res.send("Hello from express")
+}
+)
 
 app.listen(3000, () => {
     console.log('Server running at PORT: 3000')
