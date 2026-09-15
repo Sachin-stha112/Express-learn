2b6461d8d47be348002a37d770a7165a3c6c0bbb
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-15 11:30:12 +0545

    feat: add routes for about, users, products

--- phase1-express-fundamentals/server.js
+++ phase1-express-fundamentals/server.js
@@ -3,9 +3,17 @@ const app = express()
 //define a get route for home page
 app.get('/', (req,res)=> {
     res.send("Hello from express")
-}
-)
-
+})
+// get method for /about
+app.get('/about', (req, res) => {
+    res.send("Hello from about page")
+})
+app.get('/users', (req, res)=>{
+    res.send("Hello from users page")
+})
+app.get('/products', (req,res)=>{
+    res.send('Hello from products page')
+})
 app.listen(3000, () => {
     console.log('Server running at PORT: 3000')
 })
