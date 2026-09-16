7f20216d4d256ac3f385188e665a9be9a84794a0
Sachin-stha112 <coder.billionaire@gmail.com>
2026-09-16 07:55:10 +0545

    feat: add with path middleware

--- phase1-express-fundamentals/custome-middleware.js
+++ phase1-express-fundamentals/custome-middleware.js
@@ -10,13 +10,16 @@ const secondMiddleware = (req,res,next) => {
     console.log("Hello from second Middleware")
     next()
 }
-
-app.use(fistMiddleware)
-app.use(secondMiddleware)
+//registers middleware
+app.use(fistMiddleware) // no path
+app.use('/users',secondMiddleware) // with path
 
 app.get('/', (req,res) => {
     console.log('Welcome to Home page')
     res.send('Hello')
+})
+app.get('/users', (req,res)=>{
+    console.log('Hello from user page')
 })
 
 app.listen(PORT, ()=>{
